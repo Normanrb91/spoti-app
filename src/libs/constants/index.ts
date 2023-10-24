610569f44c40331c807a19820ea7cb180c0cfc9f
normanrb91 <normanromano16@gmail.com>
2023-10-24 19:18:41 +0200

    redirect uri

--- src/libs/constants/index.ts
+++ src/libs/constants/index.ts
@@ -1,7 +1,7 @@
 export const login_constants = {
     endpoint: 'https://accounts.spotify.com/authorize',
     client_id: '731f9fdb62444da58ea9c04d46b0de30',
-    redirect_uri : 'http://localhost:5173/',
+    redirect_uri : 'https://spoti-app-react.vercel.app/',
     scope : 'user-read-private user-read-email playlist-read-private'
 }
 
